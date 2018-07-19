2ffce83a827af369a2b7c97a6bd77905980d3adf
Judit Koszo <koszojudit@hotmail.com>
2018-07-19 23:50:02 +0200

    Get search function working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
 import BookList from "./components/BookList"
 import {Route, Link} from 'react-router-dom'
+import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-    * TODO: Instead of using this state variable to keep track of which page
-    * we're on, use the URL in the browser's address bar. This will ensure that
-    * users can use the browser's back and forward buttons to navigate between
-    * pages, as well as provide a good URL they can bookmark and share.
-    */
-    showSearchPage: false
+    books: [],
+    searchResults: [],
+  }
+
+  // Get the books from the API before loading main screen
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({books:books})
+    })
+  }
+
+  updateResults = (searchExpression) => {
+    if (searchExpression) {
+      BooksAPI.search(searchExpression).then(books => {
+        if (books.error) {
+          this.setState({ searchResults: [] })
+        } else {
+          this.setState({ searchResults: books })
+          console.log(books)
+        }
+      })
+    } else {
+      this.setState({ searchResults: [] })
+    }
   }
 
   render() {
+    const { books , searchResults } = this.state
     return (
       <div className="app">
 
@@ -24,16 +42,36 @@ class BooksApp extends React.Component {
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-              {/*
-                NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                You can find these search terms here:
-                https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-                However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                  <input type="text" placeholder="Search by title or author"/>
+                  <input type="text"
+                    placeholder="Search by title or author"
+                    onChange={(event) => this.updateResults(event.target.value)}
+                  />
               </div>
             </div>
+
+            <ol className="books-grid">
+              {this.state.searchResults.map((book, i) => (
+                <li key={i}>
+                  <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})` : '' }}></div>
+                        <div className="book-shelf-changer">
+                          <select>
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{book.authors ? book.authors.toString() : ' '}</div>
+                      </div>
+                    </li>
+                  ))}
+            </ol>
+
           </div>
         )}/>
 
