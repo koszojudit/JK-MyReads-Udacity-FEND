86806f5583c49258ca8d7b405c0fbb0deafb76c5
Judit Koszo <koszojudit@hotmail.com>
2018-07-19 23:58:26 +0200

    Create component for Book

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './App.css'
 import BookList from "./components/BookList"
+import Book from "./components/Book"
 import {Route, Link} from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
 
@@ -51,24 +52,7 @@ class BooksApp extends React.Component {
 
             <ol className="books-grid">
               {this.state.searchResults.map((book, i) => (
-                <li key={i}>
-                  <div className="book">
-                    <div className="book-top">
-                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: book.imageLinks ? `url(${book.imageLinks.thumbnail})` : '' }}></div>
-                        <div className="book-shelf-changer">
-                          <select>
-                            <option value="move" disabled>Move to...</option>
-                            <option value="currentlyReading">Currently Reading</option>
-                            <option value="wantToRead">Want to Read</option>
-                            <option value="read">Read</option>
-                            <option value="none">None</option>
-                            </select>
-                          </div>
-                        </div>
-                        <div className="book-title">{book.title}</div>
-                        <div className="book-authors">{book.authors ? book.authors.toString() : ' '}</div>
-                      </div>
-                    </li>
+                <Book key={i} book={book}/>
                   ))}
             </ol>
 
